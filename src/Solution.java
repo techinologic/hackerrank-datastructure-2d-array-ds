059c0fc2658e54e4376f6680009df28fca4ec3e8
Paolo T. Inocencion <paoloinocencion@gmail.com>
2017-07-21 08:07:46 -0400

    fix

--- src/Solution.java
+++ src/Solution.java
@@ -12,22 +12,32 @@ public class Solution {
             }
         }
 
-        int[] elem = new int[16];
-        int hSum = 0;
-        int n = 0;
+        int largest = Integer.MIN_VALUE;
+        int sum;
 
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-
-                hSum = array[i][j] + array[i][j + 1] + array[i][j + 2] +
+                sum = array[i][j] + array[i][j + 1] + array[i][j + 2] +
                         array[i + 1][j + 1] +
                         array[i + 2][j] + array[i + 2][j + 1] + array[i + 2][j + 2];
-                if (hSum > n) {
-                    n = hSum;
+                if (sum > largest) {
+                    largest = sum;
                 }
             }
         }
-        System.out.println(n);
+        System.out.println(largest);
     }
-
 }
+
+
+/*
+
+-1 -1 0 -9 -2 -2
+-2 -1 -6 -8 -2 -5
+-1 -1 -1 -2 -3 -4
+-1 -9 -2 -4 -4 -5
+-7 -3 -3 -2 -9 -9
+-1 -3 -1 -2 -4 -5
+
+*/
+
